c19cf93122d30b0bf0927a4886836bd46c5f10bb
Nakamura Kazutaka <kaznak.at.work@ipebble.org>
2023-04-01 16:42:52 +0900

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,19 @@
+import { invoke } from '@tauri-apps/api/tauri'
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import { useEffect } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  useEffect(() => {
+    invoke('greet', { name: 'World' })
+    .then(console.log)
+    .catch(console.error)
+  }, []);
+
   return (
     <>
       <Head>
